2f5514dbdd641808d82ff83d2468427851065e54
J.Seremak <janko2002@outlook.com>
2023-05-06 01:00:39 +0200

    Added password to testing.yml

--- utils/mysql-db.js
+++ utils/mysql-db.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'NO',
+    password: 'password',
     database: 'shareameal',
     waitForConnections: true,
     connectionLimit: 10,
